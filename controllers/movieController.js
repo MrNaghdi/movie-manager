ef8c593cfd9392fe28f059c5dfca9522602b5b44
MrNaghdi <erfanmoti9@gmail.com>
2025-08-31 07:17:18 +0330

    Complete movie schema and controller with CRUD

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,16 +1,110 @@
-//Movie controllers
-exports.getAllMovies = () => {
+const Movie = require("../models/movieModel");
+
+// get all movies
+exports.getAllMovies = async (req, res) => {
+  try {
+    const movies = await Movie.find();
+    res.status(200).json({
+      status: "Successful find movies",
+      movies: movies
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+//create new movie
+exports.createMovie = async (req, res) => {
+  try {
+    const newMovie = await Movie.create(req.body);
+    res.status(201).json({
+      status: "Successful create new movie",
+      movie: newMovie
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+//get a movie
+exports.getMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+
+    if (!movie) {
+      return res.status(404).json({
+        status: "fail",
+        message: "Movie not found",
+      });
+    }
+
+    res.status(200).json({
+      status: "Successful find movie",
+      movie,   
+    });
+      } catch (err) {
+        res.status(404).json({
+          status: "fail",
+          message: "not found movie",
+        });
+      }
+};
+
+//update a movie
+exports.updateMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findByIdAndUpdate(
+          req.params.id,       
+          req.body,          
+          { new: true, runValidators: true }  
+        );
     
-}
-exports.createmovie = () => {
+        if (!movie) {   
+          return res.status(404).json({
+            status: "fail",
+            message: "Movie not found",
+          });
+        }
+    
+        res.status(200).json({
+          status: "Movie updated successfully",
+          movie,
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "error",
+          message: err.message,
+        });
+      }
+};
 
-}
-exports.getMovie = () => {
-
-}
-exports.updateMovie = () => {
-
-}
-exports.deleteMovie = () => {
-
-}
+//delete a movie
+exports.deleteMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findByIdAndDelete(req.params.id);
+    
+        if (!movie) {
+          return res.status(404).json({
+            status: "fail",
+            message: "Movie not found",
+          });
+        }
+    
+        res.status(204).json({
+          status: "Movie deleted successfully",
+          data: null, 
+        });
+      } catch (err) {
+        res.status(500).json({
+          status: "error",
+          message: err.message,
+        });
+      }
+    
+};
---
+++ models/movieModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+
+//defind movie model
+const movieSchema = mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["movie", "series"],
+    required: [true, "a movie between movie and series"],
+  },
+  name: {
+    type: String,
+    required: [true, "a movie has a name."],
+    unique: [true, "a movie has a unique name."],
+    index: true
+  },
+  description: {
+    type: String,
+    required: [true, "a movie has a description."]
+  },
+  genres: {
+    type: [String],
+    required: [true, "a movie must have at least one genre."]
+  },
+  releaseYear: {
+    type: Number,
+    required: [true, "a movie has a release year."]
+  },
+  runtime : {
+    type: Number,
+    required: [true, "a movie has a run time."]
+  },
+  actors : {
+    type : [String],
+    required: [true, "please write actors of movie."]
+  },
+  crew: {
+    type: [
+      {
+        name: {
+          type: String,
+          required: [true, "crew member must have a name."],
+        },
+        role: {
+          type: String,
+          required: [true, "crew member must have a role."],
+        },
+      },
+    ],
+    required: [true, "a movie must have at least one crew member."],
+    validate: {
+      validator: function (array) {
+        return array.length > 0; 
+      },
+      message: "a movie must have at least one crew member.",
+    },
+  },
+  posterUrl: String,
+  trailerUrl: String,
+  ratingsAverage: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 10
+  },
+  ratingsCount: Number
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+module.exports = Movie;
